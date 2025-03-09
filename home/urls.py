a35b6caa1c5fcf6a4953cf029c2452520349c9cb
gitsokyumin <kyumin3089@gmail.com>
2025-03-09 15:13:54 +0900

    second commit

--- home/urls.py
+++ home/urls.py
@@ -7,4 +7,5 @@ app_name = 'home'
 urlpatterns = [ 
     path('', views.index, name = 'index'),
     path('<int:journal_id>/', views.detail, name = 'detail'),
+    path('comment/create/<int:journal_id>/', views.comment_create, name='comment_create'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Journal
 
 
@@ -11,3 +12,8 @@ def detail(request, journal_id):
     journal = get_object_or_404(Journal, pk= journal_id)
     context = {'journal': journal}
     return render(request, 'home/journal_detail.html', context)
+
+def comment_create(request, journal_id):
+    journal = get_object_or_404(Journal, pk= journal_id)
+    journal.comment_set.create(content=request.POST.get('content'), create_date =timezone.now())
+    return redirect('home:detail', journal_id = journal_id)
